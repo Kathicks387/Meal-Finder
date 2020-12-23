dc2dee569c958c53948c38a3be54f9dd18940608
Can Do <Katrinarhicks@gmail.com>
2020-12-23 12:10:04 -0500

    updated css created initial js file

---
+++ script.js
@@ -0,0 +1,49 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+const resultHeading = document.getElementById('result-heading');
+const mealsEl = document.getElementById('meals');
+const single_mealEl = document.getElementById('single-meal');
+
+
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+  
+  // Clear single meal
+  single_mealEl.innerHTML= '';
+
+  // Get search
+  const term = search.value;
+// Check for empty
+if(term.trim()) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    resultHeading.innerHTML = `<h2>Search result(s) for '${term}':</h2>`;
+
+    if(data.meals === null) {
+      resultHeading.innerHTML = `<p>There are no results, please try again!<p>`;
+    } else {
+      mealsEl.innerHTML = data.meals.map(meal => `
+      <div class = 'meal'>
+        <img src = '${meal.strMealThumb}' alt='${meal.strMeal}'/>
+        <div class = 'meal-info' data-mealID = '${meal.idMeal}'>
+          <h3>${meal.strMeal}</h3>
+        </div>
+      </div>
+      `)
+      .join('');
+    }
+  });
+  // Clear search text
+  search.value = '';
+
+}else {
+  alert('Please enter a search term');
+}
+}
+
+// Event Listeners
+submit.addEventListener('submit', searchMeal);
